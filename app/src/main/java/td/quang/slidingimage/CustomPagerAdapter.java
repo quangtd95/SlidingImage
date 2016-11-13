e3bacb0a0d487e0bcc43015686abe09120b476af
quang.td95 <quang.td95@gmail.com>
2016-11-13 22:59:46 +0700

    Use Picasso to load image from internet.

--- app/src/main/java/td/quang/slidingimage/CustomPagerAdapter.java
+++ app/src/main/java/td/quang/slidingimage/CustomPagerAdapter.java
@@ -1,10 +1,15 @@
 package td.quang.slidingimage;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.os.Build;
 import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -19,45 +24,30 @@ public class CustomPagerAdapter extends PagerAdapter {
         this.imageList = imageList;
     }
 
-    /**
-     * Return the number of views available.
-     */
     @Override
     public int getCount() {
-        return 0;
+        return imageList.size();
     }
 
-    /**
-     * Determines whether a page View is associated with a specific key object
-     * as returned by {@link #instantiateItem(ViewGroup, int)}. This method is
-     * required for a PagerAdapter to function properly.
-     *
-     * @param view   Page View to check for association with <code>object</code>
-     * @param object Object to check for association with <code>view</code>
-     * @return true if <code>view</code> is associated with the key object <code>object</code>
-     */
     @Override
     public boolean isViewFromObject(View view, Object object) {
-        return false;
+        return view == object;
     }
 
-    /**
-     * Create the page for the given position.  The adapter is responsible
-     * for adding the view to the container given here, although it only
-     * must ensure this is done by the time it returns from
-     * {@link #finishUpdate(ViewGroup)}.
-     *
-     * @param container The containing View in which the page will be shown.
-     * @param position  The page position to be instantiated.
-     * @return Returns an Object representing the new page.  This does not
-     * need to be a View, but can be some other container of the page.
-     */
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
         LayoutInflater layoutInflater = LayoutInflater.from(context);
         View view = layoutInflater.inflate(R.layout.layout_pager,container,false);
+        ImageView imageView = (ImageView) view.findViewById(R.id.imageView);
+        Picasso.with(context).load(imageList.get(position)).into(imageView);
 
         container.addView(view);
-        return super.instantiateItem(container, position);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
     }
 }
--- app/src/main/java/td/quang/slidingimage/MainActivity.java
+++ app/src/main/java/td/quang/slidingimage/MainActivity.java
@@ -3,6 +3,9 @@ package td.quang.slidingimage;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,8 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ViewPager pager = (ViewPager) findViewById(R.id.pager);
 
+        ArrayList<String> imageList = new ArrayList();
+        imageList.add("http://www.nolavape.com/wp-content/uploads/2013/04/honey-blossom-e-liquid-new-orleans.jpg");
+        imageList.add("http://cooldigital.photography/wp-content/uploads/2014/02/Blue-Flowers-620x444.jpg");
+        imageList.add("https://s-media-cache-ak0.pinimg.com/236x/1c/71/39/1c7139d1d7c8f54afd73ff063a4c39c3.jpg");
+        imageList.add("http://cooldigital.photography/wp-content/uploads/2014/02/Beautiful-purple-flowers-620x410.jpg");
+        imageList.add("https://s-media-cache-ak0.pinimg.com/736x/2f/ee/5f/2fee5fe4861236e8e5516c20b1da7da3.jpg");
+
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        CustomPagerAdapter pagerAdapter = new CustomPagerAdapter(this,imageList);
+        pager.setAdapter(pagerAdapter);
+
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.indicator);
 
 
     }
