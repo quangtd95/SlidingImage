d59432fbb40340c315947e5ac3ba04b4694e203e
quang.td95 <quang.td95@gmail.com>
2016-11-13 22:11:20 +0700

    Update Project

---
+++ app/src/main/java/td/quang/slidingimage/CustomPagerAdapter.java
@@ -0,0 +1,63 @@
+package td.quang.slidingimage;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+/**
+ * Created by Quang_TD on 11/13/2016.
+ */
+public class CustomPagerAdapter extends PagerAdapter {
+    private List<String> imageList;
+    private Context context;
+    public CustomPagerAdapter(Context context, List<String> imageList) {
+        this.context = context;
+        this.imageList = imageList;
+    }
+
+    /**
+     * Return the number of views available.
+     */
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    /**
+     * Determines whether a page View is associated with a specific key object
+     * as returned by {@link #instantiateItem(ViewGroup, int)}. This method is
+     * required for a PagerAdapter to function properly.
+     *
+     * @param view   Page View to check for association with <code>object</code>
+     * @param object Object to check for association with <code>view</code>
+     * @return true if <code>view</code> is associated with the key object <code>object</code>
+     */
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return false;
+    }
+
+    /**
+     * Create the page for the given position.  The adapter is responsible
+     * for adding the view to the container given here, although it only
+     * must ensure this is done by the time it returns from
+     * {@link #finishUpdate(ViewGroup)}.
+     *
+     * @param container The containing View in which the page will be shown.
+     * @param position  The page position to be instantiated.
+     * @return Returns an Object representing the new page.  This does not
+     * need to be a View, but can be some other container of the page.
+     */
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        View view = layoutInflater.inflate(R.layout.layout_pager,container,false);
+
+        container.addView(view);
+        return super.instantiateItem(container, position);
+    }
+}
